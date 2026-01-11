980547484dfff99acbd5f7a0be728d588cd9d740
Mathias <mathiasvanneste713@gmail.com>
2026-01-11 15:35:12 +0100

    Updated the App.tsx so everything is implemented

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AppNavigator from "./src/navigation/AppNavigator";
+import { ThemeProvider } from "./src/theme/ThemeContext";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <AppNavigator />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
